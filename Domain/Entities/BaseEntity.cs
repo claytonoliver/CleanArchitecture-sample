ea1018b19e1c1674f0adc79f49db3f7283eaa996
claytonOliveiratd <108222845+claytonoliver@users.noreply.github.com>
2024-10-05 19:53:01 -0300

    Implementing domain layer

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -8,7 +8,9 @@ namespace CleanArchitecture.Domain.Entities
 {
     public abstract class BaseEntity
     {
-
-
+        public Guid Id { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset? DateUpadted { get; set; }
+        public DateTimeOffset? DateDeleted { get; set; }
     }
 }
